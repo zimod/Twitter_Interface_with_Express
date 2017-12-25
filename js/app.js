0abdbe6dcb0d01b8737535ffc191f50f00183f90
Zimo Dong <andy199354@gmail.com>
2017-12-24 20:49:51 -0800

    added promises to grab all the related data from twitter API and saved them in a template

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
-const key = require('/config.js');//require the Auth tokens
+const key = require('./config.js');//require the Auth tokens
 const express = require('express'); //require express module
 const bodyParser = require('body-parser'); //using the middleware for req.body, this is for the POST method
 const Twit = require('twit');//require Twit
 
 const app = express();
+
+const T = new Twit({
+  consumer_key:         key.consumer_key,
+  consumer_secret:      key.consumer_secret,
+  access_token:         key.access_token,
+  access_token_secret:  key.access_token_secret,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -15,46 +23,30 @@ app.set('view engine', 'pug'); //set the view engine to pug so we can user it
 app.use('/static',express.static('public'));//public is the folder name, to access css file inside for example in the browser, search static/stylesheets/style.css
 
 
+//to use promise all checkout https://stackoverflow.com/questions/40643386/promise-all-in-javascript-how-to-get-resolve-value-for-all-promises
+//basic format is like:
+/* Promise.all([promise1,promise2,...])
+   .then(function([first, second,...]) {
+
+}); */
+app.get('/',(req,res)=>{
+  const template = {};//our template object to store all related information from twitter API
+  Promise.all([//see twitter API for usage https://developer.twitter.com/en/docs
+    T.get('friends/list',{count:5}),
+    T.get('statuses/user_timeline',{count:5}),
+    T.get('direct_messages',{count:5}),
+  ]).then(function([first,second,third]){//to see the reason why we use .data on the param checkout the documentation https://github.com/ttezel/twit on promises
+    template.following = first.data;
+    template.tweets = second.data;
+    template.directMsg = third.data;
+    res.send(template);
+  });
+});
 
 
 
 
-
-
-var T = new Twit({
-  consumer_key:         key.consumer_key,
-  consumer_secret:      key.consumer_secret,
-  access_token:         key.access_token,
-  access_token_secret:  key.access_token_secret,
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
-
-
-//
-//  get the list of followers, count is the limit per page
-//
-T.get('followers/list', {count:5},function (err, data, response) {
-  //console.log(data)
-})
-
-
-//
-//  get the list of following, count is the limit per page
-//
-T.get('friends/list',{count:5}, function (err, data, response) {
-  //console.log(data)
-})
-
-//
-//  get the recent tweets, count is the limit per page
-//
-T.get('statuses/user_timeline', function (err, data, response) {
-  //console.log(data);
-})
-
-//
-//get the recent direct messages, count is the limit per page
-//
-T.get('direct_messages', function (err, data, response) {
-  //console.log(data);
-})
+//create the server
+const server = app.listen(3000, () => {
+  console.log("The application is running on localhost:3000 !");
+}); //this will create a server
