7cbcc9b8c593fdc1b6802010de570aecba4d115f
Zimo Dong <andy199354@gmail.com>
2017-12-25 20:18:43 -0800

    added the logic for displaying recent messages as well as posting new tweets

--- js/app.js
+++ js/app.js
@@ -29,6 +29,8 @@ app.use('/static', express.static('public')); //public is the folder name, to ac
    .then(function([first, second,...]) {
 
 }); */
+
+//GET
 app.get('/', (req, res) => {
   const template = {}; //our template object to store all related information from twitter API
   Promise.all([ //see twitter API for usage https://developer.twitter.com/en/docs
@@ -47,16 +49,26 @@ app.get('/', (req, res) => {
   ]).then(function([userInfo, followingInfo, timelineInfo, directMsgInfo]) { //to see the reason why we use .data on the param checkout the documentation https://github.com/ttezel/twit on promises
     getAllData(template, userInfo, followingInfo, timelineInfo, directMsgInfo); //use the helper function to collect raw data and store in template
     getRecentTweets(template, template.tweets); //using the template.tweets property, get the recent tweets text and store them in template.recentTweets
-    getRecentFriends(template, template.following.users);///using the template.following.users property, get the recent following store them in template.recentFriends
-    getRecentDirectMsg(template,template.directMsg);
+    getRecentFriends(template, template.following.users); ///using the template.following.users property, get the recent following store them in template.recentFriends
+    getRecentDirectMsg(template, template.directMsg); //using the template.directMsg property, get the recent Dms store them in template.recentDirectMsg
     res.render('main', { //render main.pug with given params
       username: template.user.name,
       screen_name: template.user.screen_name,
       profile_image_url: template.user.profile_image_url_https,
       following_count: template.user.friends_count,
       recentTweets: template.recentTweets,
-      recentFriends: template.recentFriends
+      recentFriends: template.recentFriends,
+      recentDirectMsg: template.recentDirectMsg
     });
+  });
+});
+
+//POST
+app.post('/new', (req, res) => {
+  T.post('statuses/update', {
+    status: req.body.userinput
+  }).then(function(result){
+     res.redirect('/');
   });
 });
 
@@ -70,39 +82,40 @@ const getAllData = (result, item1, item2, item3, item4) => { //helper function t
 
 const getRecentTweets = (result, data) => { // a helper function to grab recent 5 tweets  from data and store them in result.recentTweets
   result.recentTweets = [];
-  data.forEach(function(item) {//checkout https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline
+  data.forEach(function(item) { //checkout https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline
     result.recentTweets.push({
       TweetText: item.text,
       Retweet: item.retweet_count,
       Like: item.favorite_count,
-      CreatedAt:item.created_at
+      CreatedAt: item.created_at
     });
   });
   //console.log(result.recentTweets);
 };
 
-const getRecentFriends = (result,data)=>{// a helper function to grab recent 5 followings from data and store them in result.recentFriends
-   result.recentFriends = [];
-   data.forEach(function(item){//checkout https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/get-friends-list
-     result.recentFriends.push({
-        friendName: item.name,
-        friendScreenName: item.screen_name,
-        friendProfileIMG: item.profile_image_url_https
-     });
-   });
-   //console.log(result.recentFriends);
+const getRecentFriends = (result, data) => { // a helper function to grab recent 5 followings from data and store them in result.recentFriends
+  result.recentFriends = [];
+  data.forEach(function(item) { //checkout https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/get-friends-list
+    result.recentFriends.push({
+      friendName: item.name,
+      friendScreenName: item.screen_name,
+      friendProfileIMG: item.profile_image_url_https
+    });
+  });
+  //console.log(result.recentFriends);
 };
 
-const getRecentDirectMsg = (result,data)=>{// a helper function to grab recent 5 Dms from data and store them in result.recentFriends
-    result.recentDirectMsg = [];
-    data.forEach(function(item){ //checkout https://developer.twitter.com/en/docs/direct-messages/sending-and-receiving/api-reference/get-messages
-       result.recentDirectMsg.push({
-         recipientName:item.recipient.name,
-         createdAt : item.recipient.created_at,
-         message: item.recipient.description
-       });
+const getRecentDirectMsg = (result, data) => { // a helper function to grab recent 5 Dms from data and store them in result.recentFriends
+  result.recentDirectMsg = [];
+  data.forEach(function(item) { //checkout https://developer.twitter.com/en/docs/direct-messages/sending-and-receiving/api-reference/get-messages
+    result.recentDirectMsg.push({
+      recipientName: item.sender.name,
+      createdAt: item.created_at,
+      message: item.text,
+      recipientProfileImg: item.sender.profile_image_url_https
     });
-    console.log(result.recentDirectMsg);
+  });
+  //console.log(result.recentDirectMsg);
 };
 
 //create the server
