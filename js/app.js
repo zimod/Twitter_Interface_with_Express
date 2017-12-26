5cc128de84b59b984df9be8ceb73a0acd4692e55
Zimo Dong <andy199354@gmail.com>
2017-12-25 16:40:30 -0800

    added the logic for displaying timeline.pug

--- js/app.js
+++ js/app.js
@@ -32,6 +32,9 @@ app.use('/static', express.static('public')); //public is the folder name, to ac
 app.get('/', (req, res) => {
   const template = {}; //our template object to store all related information from twitter API
   Promise.all([ //see twitter API for usage https://developer.twitter.com/en/docs
+    T.get('account/verify_credentials', {
+      skip_status: true
+    }),
     T.get('friends/list', {
       count: 5
     }),
@@ -41,16 +44,39 @@ app.get('/', (req, res) => {
     T.get('direct_messages', {
       count: 5
     }),
-  ]).then(function([first, second, third]) { //to see the reason why we use .data on the param checkout the documentation https://github.com/ttezel/twit on promises
-    template.following = first.data;
-    template.tweets = second.data;
-    template.directMsg = third.data;
-    res.render('main');
+  ]).then(function([userInfo, followingInfo, timelineInfo, directMsgInfo]) { //to see the reason why we use .data on the param checkout the documentation https://github.com/ttezel/twit on promises
+    getAllData(template, userInfo, followingInfo, timelineInfo, directMsgInfo); //use the helper function to collect raw data and store in template
+    getRecentTweets(template, template.tweets); //using the template.tweets property, get the recent tweets text and store them in template.recentTweets
+    res.render('main', { //render main.pug with given params
+      username: template.user.name,
+      screen_name: template.user.screen_name,
+      profile_image_url: template.user.profile_image_url_https,
+      following_count: template.user.friends_count,
+      recentTweets: template.recentTweets
+    });
   });
 });
 
 
+const getAllData = (result, item1, item2, item3, item4) => { //helper function to retrieve all needed data and store them in the result object
+  result.user = item1.data;
+  result.following = item2.data;
+  result.tweets = item3.data;
+  result.directMsg = item4.data;
+};
 
+const getRecentTweets = (result, data) => { // a helper function to grab recent 5 tweets  from data and store them in result.recentTweets
+  result.recentTweets = [];
+  data.forEach(function(item) {//checkout https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline
+    result.recentTweets.push({
+      TweetText: item.text,
+      Retweet: item.retweet_count,
+      Like: item.favorite_count,
+      CreatedAt:item.created_at
+    });
+  });
+  console.log(result.recentTweets);
+};
 
 //create the server
 const server = app.listen(3000, () => {
